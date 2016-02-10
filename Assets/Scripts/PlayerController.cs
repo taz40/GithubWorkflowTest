a65b9d31c04a777b6848af217b803be298787945
BuildTools <unconfigured@null.spigotmc.org>
2016-02-09 18:49:56 -0700

    added character controller script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    CharacterController cc;
+    Vector3 movement;
+    float verticalVel;
+    float speed = 0;
+
+	// Use this for initialization
+	void Start () {
+        cc = this.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        verticalVel += Physics.gravity.y * Time.deltaTime;
+        /*if (cc.isGrounded && verticalVel < Physics.gravity.y) {
+            verticalVel = Physics.gravity.y;
+        }*/  // do not uncomment, this is to test the hotfix.
+
+        movement.z += Input.GetAxis("Vertical")*Time.deltaTime*speed;
+        movement.x += Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+        
+	}
+
+    void FixedUpdate() {
+        
+        cc.Move(transform.rotation*movement);
+        movement = Vector3.zero;
+    }
+}
